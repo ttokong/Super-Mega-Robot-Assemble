889a3ed44bf05b12cb798d46e94047eb8b5d27c1
Park Yu Seung <ttokong2486@gmail.com>
2020-04-20 23:27:06 +0800

    Basic Player Control Setup

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PlayerStats
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+
+    void InitGame()
+    {
+        OGhealth = health;
+        CC = gameObject.GetComponent<CharacterController>();
+    }
+
+
+
+    void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        CC.Move(move * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float speed;
+
+    public float health;
+    public float ultiPercentage;
+    
+
+    [HideInInspector]
+    public float OGhealth;
+
+    [HideInInspector]
+    public CharacterController CC;
+
+    public void TakeDamage(float dmg)
+    {
+        health -= dmg;
+    }
+}

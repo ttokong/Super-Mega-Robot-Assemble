5c2d958366b34569b120c41623c47b34bec20bc9
Park Yu Seung <ttokong2486@gmail.com>
2020-04-27 02:33:39 +0800

    Added Basic Script for robot controls

--- Assets/Scripts/Player/Robot/RobotController.cs
+++ Assets/Scripts/Player/Robot/RobotController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class RobotController : MonoBehaviour
 {
@@ -9,16 +10,72 @@ public class RobotController : MonoBehaviour
     public GameObject Arms_R;
     public GameObject Torso;
 
+    public int playerID;
 
-    // Start is called before the first frame update
-    void Start()
+
+    private PlayerControls controls;
+    private Camera cam;
+    public float allowRotation;
+    private Vector2 movementInput;
+    private Vector3 dir;
+
+
+    void Awake()
     {
-        
+        cam = Camera.main;
+        controls = new PlayerControls();
+        controls.Gameplay.Move.performed += ctx => movementInput = ctx.ReadValue<Vector2>();        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        InputDecider();                             // dont touch this thanks
+    }
+
+
+    void InputDecider()
+    {
+        if (playerID == 1 && playerID == 2)
+        {
+            float currentSpeed = new Vector2(movementInput.x, movementInput.y).sqrMagnitude;
+
+            if (currentSpeed > allowRotation)                   //if u exceed a certain speed u will rotate basically
+            {
+                Rotation();
+            }
+            else
+            {
+                dir = Vector3.zero;                             //if not moving then dont rotate
+            }
+        }
+    }
+
+
+    void Rotation()                 //this part makes u rotate to face the direction of movement
+    {
+
+        Vector3 forward = cam.transform.forward;
+        Vector3 right = cam.transform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        dir = right * movementInput.x + forward * movementInput.y;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 0.15F);
+    }
+
+
+    public void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    public void OnDisable()
+    {
+        controls.Disable();
     }
 }

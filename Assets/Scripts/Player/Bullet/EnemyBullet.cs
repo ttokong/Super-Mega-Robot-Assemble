d697d9e3257a98aa05892f4f5c7b8d8741b43c5f
Park Yu Seung <ttokong2486@gmail.com>
2020-04-27 01:49:38 +0800

    Basic Robot Setup

---
+++ Assets/Scripts/Player/Bullet/EnemyBullet.cs
@@ -0,0 +1,61 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    private PhotonView PV;
+
+    private float bulletSpeed = 100;
+
+    public float bulletSpeedMultiplier;
+
+    public float damage;
+
+    public GameObject impactExplosion;
+
+    private Rigidbody bullet;
+
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+
+        bullet = gameObject.GetComponent<Rigidbody>();
+
+        bulletSpeed *= bulletSpeedMultiplier;
+
+    }
+
+    private void Update()
+    {
+        bullet.velocity = transform.forward * bulletSpeed * Time.deltaTime;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Enemy" && other.tag != "Bullet")
+        {
+            if (other.tag == "Player")
+            {
+                other.GetComponent<PhotonView>().RPC("RPC_PlayerTakeDamage", RpcTarget.All, damage);
+
+                PV.RPC("DestroyBullet", RpcTarget.All);
+            }
+            else
+            {
+                PV.RPC("DestroyBullet", RpcTarget.All);
+            }
+        }
+    }
+
+    [PunRPC]
+    private void DestroyBullet()
+    {
+        Destroy(gameObject);
+
+        GameObject effect = Instantiate(impactExplosion, transform.position, transform.rotation);
+        Destroy(effect, 3f);
+    }
+}
---
+++ Assets/Scripts/Player/Robot/RobotController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+    public GameObject Legs;
+    public GameObject Arms_L;
+    public GameObject Arms_R;
+    public GameObject Torso;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

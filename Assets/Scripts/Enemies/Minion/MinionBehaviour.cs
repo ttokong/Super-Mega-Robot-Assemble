75077b0280ada687542b9e3badd86eb7bf083bb1
Park Yu Seung <ttokong2486@gmail.com>
2020-08-07 11:41:33 +0800

    upgraded render pipeline to HDRP

--- Assets/Scripts/Enemies/Minion/MinionBehaviour.cs
+++ Assets/Scripts/Enemies/Minion/MinionBehaviour.cs
@@ -84,6 +84,7 @@ public class MinionBehaviour : EnemyParameters
         {
             Follow();
         }
+        //add evade feature
         else
         {
             actionComplete = true;

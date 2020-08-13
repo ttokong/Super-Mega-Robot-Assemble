86b013734b10537dee737629c8764fd75c71c82c
Kang Zi Yang <kzy22.18@ichat.sp.edu.sg>
2020-08-14 02:08:40 +0800

    tutorial sequence done

--- Assets/Scripts/Managers/TutorialManager.cs
+++ Assets/Scripts/Managers/TutorialManager.cs
@@ -34,7 +34,42 @@ public class TutorialManager : MonoBehaviour
         if (popUpIndex == 0)
         {
             // checks whether any input of this specific button is received
-            // if(Input.)
+            if(Input.GetAxis("Move") != 0)
+            {
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 1)
+        {
+            if (Input.GetAxis("Aim") != 0)
+            {
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 2)
+        {
+            if (Input.GetAxis("ShootHold") != 0)
+            {
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 3)
+        {
+            if (Input.GetAxis("Ultimate") != 0)
+            {
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 4)
+        {
+            if (Input.GetAxis("Super Mega Robot Assemble") != 0)
+            {
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 5)
+        {
+            if (Input.GetAxis("Ultimate") != 0)
             {
                 popUpIndex++;
             }

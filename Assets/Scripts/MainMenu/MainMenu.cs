fd608ac7e1bc42c5914b15d596510ed77d8381e4
Glenn Soe <gsjzd123@gmail.com>
2020-08-15 00:19:00 +0800

    Main Menu Done

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -5,8 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private bool flash;
 
     public GameObject lobby;
+    public GameObject options;
+    public GameObject credits;
     public GameObject mainMenu;
 
     public void OpenLobby()
@@ -15,8 +18,75 @@ public class MainMenu : MonoBehaviour
         mainMenu.SetActive(false);
     }
 
+    public void OpenOptions()
+    {
+        options.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void OpenCredits()
+    {
+        credits.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public IEnumerator FlashCOStart(int secs)
+    {
+        yield return new WaitForSeconds(secs);
+        OpenLobby();
+        GameObject.Find("PlayerConfigurationManager").GetComponent<PlayerManager>().ActivateScript();
+    }
+
+    public IEnumerator FlashCOOptions(int secs)
+    {
+        yield return new WaitForSeconds(secs);
+        OpenOptions();
+    }
+
+    public IEnumerator FlashCOCredits(int secs)
+    {
+        yield return new WaitForSeconds(secs);
+        OpenCredits();
+
+    }
+
+    public void FlashStart(int sec)
+    {
+        if (!flash)
+        {
+            StartCoroutine(FlashCOStart(sec));
+        }
+        else if (flash)
+            return;
+    }
+
+    public void FlashOptions(int sec)
+    {
+        if (!flash)
+        {
+            StartCoroutine(FlashCOOptions(sec));
+        }
+        else if (flash)
+            return;
+    }
+
+    public void FlashCredits(int sec)
+    {
+        if (!flash)
+        {
+            StartCoroutine(FlashCOCredits(sec));
+        }
+        else if (flash)
+            return;
+    }
+
+    public void BTMM()
+    {
+        flash = false;
     }
 }

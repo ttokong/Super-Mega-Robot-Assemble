7fb7542378740f81bb777fd94e7c69cf98bc5563
Kang Zi Yang <kzy22.18@ichat.sp.edu.sg>
2020-08-20 12:52:18 +0800

    stage system

--- Assets/StageSystem.cs
+++ Assets/StageSystem.cs
@@ -6,6 +6,10 @@ public class StageSystem : MonoBehaviour
 {
     public GameObject Boss;
     public GameObject VictoryScreen;
+    public GameObject Text0;
+    public GameObject Text1;
+    public GameObject Text2;
+    public GameObject Text3;
 
     int PhaseCounter = 0;
     public static int MinionsKilled;
@@ -14,6 +18,9 @@ public class StageSystem : MonoBehaviour
     {
         Boss.SetActive(false);
         VictoryScreen.SetActive(false);
+        Text1.SetActive(false);
+        Text2.SetActive(false);
+        Text3.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,14 +28,17 @@ public class StageSystem : MonoBehaviour
     {
         if (PhaseCounter == 0 && MinionsKilled == 5)
         {
+            StartCoroutine(Phase1());
             EnterPhase1();
         }
         else if (PhaseCounter == 1 && MinionsKilled == 20)
         {
+            StartCoroutine(Phase2());
             EnterPhase2();
         }
         else if (PhaseCounter == 2 && Boss.GetComponent<BossBehaviour>().enraged == true)
         {
+            StartCoroutine(Phase3());
             EnterPhase3();
         }
 
@@ -55,5 +65,42 @@ public class StageSystem : MonoBehaviour
             FindObjectOfType<AudioManager>().Play("BossDead");
             VictoryScreen.SetActive(true);
         }
+    }
+
+
+    IEnumerator Phase0()
+    {
+        Time.timeScale = 0;
+        Text0.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Time.timeScale = 1;
+        Text0.SetActive(false);
+    }
+
+    IEnumerator Phase1()
+    {
+        Time.timeScale = 0;
+        Text1.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Time.timeScale = 1;
+        Text1.SetActive(false);
+    }
+
+    IEnumerator Phase2()
+    {
+        Time.timeScale = 0;
+        Text2.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Time.timeScale = 1;
+        Text2.SetActive(false);
+    }
+
+    IEnumerator Phase3()
+    {
+        Time.timeScale = 0;
+        Text3.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Time.timeScale = 1;
+        Text3.SetActive(false);
     }
 }

bcc27ac97d9ba0c10d353b9a5ad07e867ea324d9
Kang Zi Yang <kzy22.18@ichat.sp.edu.sg>
2020-08-20 12:30:49 +0800

    MM fix

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
@@ -11,23 +14,89 @@ public class MainMenu : MonoBehaviour
     public GameObject options;
     public GameObject credits;
     public GameObject mainMenu;
+    public Sprite[] UiSprites;
+    public GameObject[] UiImagesToChange;
+
+    public Button StartButton;
+    Gamepad gp;
+
+    public Dropdown firstSelected;
+
+    private bool notOnMainMenu;
+    private bool onLobby;
+
+    private void Start()
+    {
+        gp = InputSystem.GetDevice<Gamepad>();
+    }
+    private void Update()
+    {
+        if (notOnMainMenu)
+        {
+            if (!onLobby)
+            {
+                if(gp.buttonEast.wasPressedThisFrame)
+                {
+                    onLobby = false;
+                    notOnMainMenu = false;
+                    options.SetActive(false);
+                    credits.SetActive(false);
+                    lobby.SetActive(false);
+                    mainMenu.SetActive(true);
+                    for (int i = 0; i < UiSprites.Length; i++)
+                    {
+                        UiImagesToChange[i].GetComponent<Image>().sprite = UiSprites[i];
+                    }
+                    StartButton.Select();
+                }
+            }
+            else if (onLobby)
+            {
+                if (PlayerManager.instance.GetPlayerConfigurations().All(p => p.IsReady == false))
+                {
+                    if (gp.buttonEast.wasPressedThisFrame)
+                    {
+                        onLobby = false;
+                        notOnMainMenu = false;
+                        options.SetActive(false);
+                        credits.SetActive(false);
+                        lobby.SetActive(false);
+                        mainMenu.SetActive(true);
+                        for (int i = 0; i < UiSprites.Length; i++)
+                        {
+                            UiImagesToChange[i].GetComponent<Image>().sprite = UiSprites[i];
+                            Debug.Log(i);
+                        }
+                        StartButton.Select();
+                    }
+                }
+                else { return; }
+            }
+        }
+    }
 
     public void OpenLobby()
     {
+        onLobby = true;
         lobby.SetActive(true);
         mainMenu.SetActive(false);
     }
 
     public void OpenOptions()
     {
+        onLobby = false;
         options.SetActive(true);
         mainMenu.SetActive(false);
+        notOnMainMenu = true;
     }
 
     public void OpenCredits()
     {
+        onLobby = false;
         credits.SetActive(true);
+        firstSelected.Select();
         mainMenu.SetActive(false);
+        notOnMainMenu = true;
     }
 
     public void QuitGame()
